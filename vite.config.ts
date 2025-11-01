3bf502f4e9a3f13fb64e0632e9561f2c6d1430cb
Francois Gergaud <francoisgergaud@gmail.com>
2025-11-01 15:46:58 +0100

    deploy build to GitHub page

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '', //remove the leading '/' in the src attribute in the built
   plugins: [
     vue(),
     vueJsx(),
